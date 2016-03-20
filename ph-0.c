056d58da962bec834c25c7de67c4765c66b4fbc4
Brent Pinkney <bp@ffroot.co.za>
2016-03-20 07:30:20 +0200

    Add exit code to halt, halt if mmap fails.

--- ph-0.c
+++ ph-0.c
@@ -12,11 +12,11 @@ int verbose = 1;
 
 typedef void cell;
 
-static cell * halt( cell * n )
+static cell * halt( unsigned long n, cell * env )
 {
 	dprintf( "halting…\n" );
-	exit( 1 );
-	return n;
+	exit( n );
+	return env;
 }
 
 static cell * sire( cell * ignore )
@@ -28,12 +28,17 @@ static cell * sire( cell * ignore )
 			PROT_READ | PROT_WRITE | PROT_EXEC,
 			MAP_ANONYMOUS | MAP_PRIVATE,
 			0, 0 );
+	if( arena == MAP_FAILED )
+	{
+		halt( 1, ignore );
+	}
 	dprintf( "size: %ld, arena: %p, last: %p\n", size, arena, arena + size - 1 );
+
 	return arena;
 }
 
 int main( )
 {
-	halt( sire( 0 ) );
+	halt( 0, sire( 0 ) );
 	return 0;
 }
