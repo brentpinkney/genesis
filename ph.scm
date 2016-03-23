27ec92e64df8e727701dbd136e64adda270ab0f5
Brent Pinkney <bp@ffroot.co.za>
2016-03-23 20:25:51 +0200

    Added.

---
+++ ph.scm
@@ -0,0 +1,13 @@
+;;; pre-history
+
+;;; (~E) (not e)
+(!~(^(E)(?E()0xff)))
+
+;;; (v)	= (environment)
+(!v($(EV)V))
+
+;;; ('E)  = (quote e)
+(!'($(EV)E))
+
+;;; (kEFGH…) (list e f g h …)
+(!k ($ (EV) (%E) ))
