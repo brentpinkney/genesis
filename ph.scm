3ab6ef1eab880ab013af2055c4540dd18a9107d8
Brent Pinkney <bp@ffroot.co.za>
2016-03-24 08:53:41 +0200

    Reorder and add map.

--- ph.scm
+++ ph.scm
@@ -1,14 +1,17 @@
 ;;; pre-history
 
-;;; (kEFGH…) (list e f g h …)
-(!k(^(E)(?(tE)E(.E()))))
-
-;;; (~E) (not e)
-(!~(^(E)(?E()0xff)))
-
 ;;; (v)	= (environment)
 (!v($(EV)V))
 
 ;;; ('E)  = (quote e)
 (!'($(EV)E))
+
+;;; (~E) (not e)
+(!~(^(E)(?E()0x00)))
+
+;;; (kEFGH…) (list e f g h …)
+(!k(^(E)(?(tE)E(.E()))))
+
+;;; (mFL) (map f l)
+(! m (^ (F L) (? (_ L) () (. (: F (# L)) (m F (% L))))))
 
